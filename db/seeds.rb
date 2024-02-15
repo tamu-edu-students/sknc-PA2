5aa33c74864f321e13c2f8abc657602d8e675256
schandhr <sknc@tamu.edu>
2024-02-15 13:59:02 -0600

    Added 3 more movies to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,15 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'The Last Duel', :rating => 'R',
+    :release_date => '15-Oct-2021'},
+  {:title => 'Avatar: The Way of Water', :rating => 'PG-13',
+    :release_date => '16-Dec-2022'},
+  {:title => 'Oppenheimer', :rating => 'R',
+    :release_date => '21-Jul-2023'}
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by(movie)
 end
